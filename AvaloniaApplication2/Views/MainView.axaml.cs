bcfdeeda8bc9d67d6a83e00375eb4e58cbf40ea1
Banko Hadjiyski <banko12@users.noreply.github.com>
2026-06-03 10:48:45 -0500

    yay,works in browser

--- AvaloniaApplication2/Views/MainView.axaml.cs
+++ AvaloniaApplication2/Views/MainView.axaml.cs
@@ -1,5 +1,14 @@
 using Avalonia.Controls;
 
+
+using B;
+
+using B.NA.App.Facade;
+using B.NA.Ux3.Facade;
+using B.Ux;
+
+
+
 namespace AvaloniaApplication2.Views;
 
 public partial class MainView : UserControl
@@ -7,5 +16,17 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+
+        var dp = new DockPanel();
+        this.Content = dp;
+
+        var sp = uin.StackPanel(width: 180).AddRight(dp);
+        ui.LoggerWithCopyAndClear().SetGlobalLogger().AddTo(dp);
+        int i = 0;
+        ui.Btn("Run").AddTo(sp).WithClickEx(async () =>
+        {
+            $"hello {i++}".Log();
+        });
+
     }
 }
