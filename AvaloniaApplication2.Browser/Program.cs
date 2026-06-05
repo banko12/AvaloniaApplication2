68fd8ea35b159ab0f1d5d5e2504cbbbda5836c2d
Banko Hadjiyski <banko12@users.noreply.github.com>
2026-06-05 11:47:06 -0500

    tidy up

--- AvaloniaApplication2.Browser/Program.cs
+++ AvaloniaApplication2.Browser/Program.cs
@@ -6,19 +6,18 @@ using System.Threading.Tasks;
 
 internal sealed partial class Program
 {
-    private static async Task Main(string[] args)
+    static async Task Main(string[] args)
     {
         WebSerial.SetCurrent(new BrowserWebSerial());
 
-        await BuildAvaloniaApp()
+        await GetBuilder()
         .StartBrowserAppAsync("out");  //the div id in index.html to mount the app to
 
     }
 
-    public static AppBuilder BuildAvaloniaApp()
+    static AppBuilder GetBuilder()
     { 
         var builder =  AppBuilder.Configure<App>()
-                        .WithInterFont()
                         #if DEBUG
                         .WithDeveloperTools()
                         #endif
