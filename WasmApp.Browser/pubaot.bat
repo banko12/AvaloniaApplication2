e3acf64f713149719a15e7cd80a7dc5479870c51
Banko Hadjiyski <banko12@users.noreply.github.com>
2026-06-09 17:08:28 -0500

    cleanup

--- WasmApp.Browser/pubaot.bat
+++ WasmApp.Browser/pubaot.bat
@@ -1,4 +1,4 @@
-dotnet publish -c Release -o .\bin\wasmaot -p:RunAOTCompilation=true -p:WasmStripILAfterAOT=true
+dotnet publish -c Release -o .\bin\wasmaot -p:RunAOTCompilation=true -p:WasmStripILAfterAOT=true -p:PublishTrimmed=true
 
 explorer .\bin\wasmaot
 :: dotnet serve -o
